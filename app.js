7417f0aa87740f9b6c3c44a0419ecd4d4feb09c2
nikkoprasetyo <nikko.prasety580@gmail.com>
2022-07-23 20:32:40 +0700

    Server init. Making the main js

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const express = require('express')
+const port = 3000
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+app.get('/', (req, res) => {
+    res.send("HELLLLLLLLLLOOOOOOO") //Testing for Api
+})
+
+app.use(cors())
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
